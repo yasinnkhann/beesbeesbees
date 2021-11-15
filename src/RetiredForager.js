c18294ce36b6d5c50316ebe5386e71a3a5a7dcd6
Yasin Khan <yasinkhan500@gmail.com>
2021-11-15 11:02:08 -0800

    finished

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,20 @@
 var RetiredForagerBee = function() {
+  Grub.call(this);
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
